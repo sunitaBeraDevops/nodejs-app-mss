59109f2f7cf3953d619d857b72bbebe3ab86608f
sunitaBeraDevops <139767645+sunitaBeraDevops@users.noreply.github.com>
2023-08-25 13:42:06 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.2.5.251:9000/',
+  serverUrl: 'http://172.31.6.103:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_c38245de6343beea3b9a299702af209e39861208',
+	    'sonar.login': 'squ_3e3fc23e726699a816e74db74d19c19fad030f6b',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
